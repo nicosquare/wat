4c085f50537f2aa4b670f225046b9f9bf3158985
Nicolas Cuadrado <nicolas.cuadrado@hotmail.com>
2017-11-05 10:25:31 +0100

    Full basic button disposition

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,145 +1,166 @@
-$( document ).ready(function() {
-	
-	var main_zones = [false,false,false];
-	
-    
-	$("#zone-1").click(function(){
-		
-		if(!main_zones[0]){
-			
-			if(!main_zones[1] && !main_zones[2]){
-				$("#house-principal").attr("src","img/house_1_0_0.png");
+$(document).ready(function() {
+
+	var main_zones = [ false, false, false ];
+
+	$("#zone-1").click(function() {
+
+		if (!main_zones[0]) {
+
+			if (!main_zones[1] && !main_zones[2]) {
+				$("#house-principal").attr("src", "img/house_1_0_0.png");
 			}
-			if(!main_zones[1] && main_zones[2]){
-				$("#house-principal").attr("src","img/house_1_0_3.png");
-			} 
-			if(main_zones[1] && !main_zones[2]){
-				$("#house-principal").attr("src","img/house_1_2_0.png");
-			} else
-			if(main_zones[1] && main_zones[2]){
-				$("#house-principal").attr("src","img/house_1_2_3.png");
+			if (!main_zones[1] && main_zones[2]) {
+				$("#house-principal").attr("src", "img/house_1_0_3.png");
 			}
-			
+			if (main_zones[1] && !main_zones[2]) {
+				$("#house-principal").attr("src", "img/house_1_2_0.png");
+			} else if (main_zones[1] && main_zones[2]) {
+				$("#house-principal").attr("src", "img/house_1_2_3.png");
+			}
+
 			main_zones[0] = true;
-			
-		}
-		else {
-			
-			if(!main_zones[1] && !main_zones[2]){
-				$("#house-principal").attr("src","img/house_0_0_0.png");
+
+		} else {
+
+			if (!main_zones[1] && !main_zones[2]) {
+				$("#house-principal").attr("src", "img/house_0_0_0.png");
 			}
-			if(!main_zones[1] && main_zones[2]){
-				$("#house-principal").attr("src","img/house_0_0_3.png");
+			if (!main_zones[1] && main_zones[2]) {
+				$("#house-principal").attr("src", "img/house_0_0_3.png");
 			}
-			if(main_zones[1] && !main_zones[2]){
-				$("#house-principal").attr("src","img/house_0_2_0.png");
+			if (main_zones[1] && !main_zones[2]) {
+				$("#house-principal").attr("src", "img/house_0_2_0.png");
 			}
-			if(main_zones[1] && main_zones[2]){
-				$("#house-principal").attr("src","img/house_0_2_3.png");
+			if (main_zones[1] && main_zones[2]) {
+				$("#house-principal").attr("src", "img/house_0_2_3.png");
 			}
 
 			main_zones[0] = false;
-			
+
 		}
-		
+
 	})
-	
-	$("#zone-2").click(function(){
-		
-		if(!main_zones[1]){
-			
-			if(!main_zones[0] && !main_zones[2]){
-				$("#house-principal").attr("src","img/house_0_2_0.png");
+
+	$("#zone-2").click(function() {
+
+		if (!main_zones[1]) {
+
+			if (!main_zones[0] && !main_zones[2]) {
+				$("#house-principal").attr("src", "img/house_0_2_0.png");
 			}
-			if(!main_zones[0] && main_zones[2]){
-				$("#house-principal").attr("src","img/house_0_2_3.png");
-			} 
-			if(main_zones[0] && !main_zones[2]){
-				$("#house-principal").attr("src","img/house_1_2_0.png");
-			} else
-			if(main_zones[0] && main_zones[2]){
-				$("#house-principal").attr("src","img/house_1_2_3.png");
+			if (!main_zones[0] && main_zones[2]) {
+				$("#house-principal").attr("src", "img/house_0_2_3.png");
 			}
-			
+			if (main_zones[0] && !main_zones[2]) {
+				$("#house-principal").attr("src", "img/house_1_2_0.png");
+			} else if (main_zones[0] && main_zones[2]) {
+				$("#house-principal").attr("src", "img/house_1_2_3.png");
+			}
+
 			main_zones[1] = true;
-			
-		}
-		else {
-			
-			if(!main_zones[0] && !main_zones[2]){
-				$("#house-principal").attr("src","img/house_0_0_0.png");
+
+		} else {
+
+			if (!main_zones[0] && !main_zones[2]) {
+				$("#house-principal").attr("src", "img/house_0_0_0.png");
 			}
-			if(!main_zones[0] && main_zones[2]){
-				$("#house-principal").attr("src","img/house_0_0_3.png");
+			if (!main_zones[0] && main_zones[2]) {
+				$("#house-principal").attr("src", "img/house_0_0_3.png");
 			}
-			if(main_zones[0] && !main_zones[2]){
-				$("#house-principal").attr("src","img/house_1_0_0.png");
+			if (main_zones[0] && !main_zones[2]) {
+				$("#house-principal").attr("src", "img/house_1_0_0.png");
 			}
-			if(main_zones[0] && main_zones[2]){
-				$("#house-principal").attr("src","img/house_1_0_3.png");
+			if (main_zones[0] && main_zones[2]) {
+				$("#house-principal").attr("src", "img/house_1_0_3.png");
 			}
 
 			main_zones[1] = false;
-			
-		}
-		
-	})
-	
-	$("#zone-3").click(function(){
 
-		if(!main_zones[2]){
-			
-			if(!main_zones[1] && !main_zones[0]){
-				$("#house-principal").attr("src","img/house_0_0_3.png");
-			}
-			if(!main_zones[1] && main_zones[0]){
-				$("#house-principal").attr("src","img/house_1_0_3.png");
-			} 
-			if(main_zones[1] && !main_zones[0]){
-				$("#house-principal").attr("src","img/house_0_2_3.png");
-			} else
-			if(main_zones[1] && main_zones[0]){
-				$("#house-principal").attr("src","img/house_1_2_3.png");
-			}
-			
-			main_zones[2] = true;
-			
 		}
-		else {
-			
-			if(!main_zones[1] && !main_zones[0]){
-				$("#house-principal").attr("src","img/house_0_0_0.png");
+
+	})
+
+	$("#zone-3").click(function() {
+
+		if (!main_zones[2]) {
+
+			if (!main_zones[1] && !main_zones[0]) {
+				$("#house-principal").attr("src", "img/house_0_0_3.png");
 			}
-			if(!main_zones[1] && main_zones[0]){
-				$("#house-principal").attr("src","img/house_1_0_0.png");
+			if (!main_zones[1] && main_zones[0]) {
+				$("#house-principal").attr("src", "img/house_1_0_3.png");
 			}
-			if(main_zones[1] && !main_zones[0]){
-				$("#house-principal").attr("src","img/house_0_2_0.png");
+			if (main_zones[1] && !main_zones[0]) {
+				$("#house-principal").attr("src", "img/house_0_2_3.png");
+			} else if (main_zones[1] && main_zones[0]) {
+				$("#house-principal").attr("src", "img/house_1_2_3.png");
 			}
-			if(main_zones[1] && main_zones[0]){
-				$("#house-principal").attr("src","img/house_1_2_0.png");
+
+			main_zones[2] = true;
+
+		} else {
+
+			if (!main_zones[1] && !main_zones[0]) {
+				$("#house-principal").attr("src", "img/house_0_0_0.png");
+			}
+			if (!main_zones[1] && main_zones[0]) {
+				$("#house-principal").attr("src", "img/house_1_0_0.png");
+			}
+			if (main_zones[1] && !main_zones[0]) {
+				$("#house-principal").attr("src", "img/house_0_2_0.png");
+			}
+			if (main_zones[1] && main_zones[0]) {
+				$("#house-principal").attr("src", "img/house_1_2_0.png");
 			}
 
 			main_zones[2] = false;
-			
+
 		}
-		
+
+	})
+
+	$("#button-send").click(function() {
+
+		$("#button-send").attr("src", "img/send_energy_on.png");
+		$("#house-alt").attr("src", "img/house_alt_on.png");
+
+		var timer = setTimeout(function() {
+
+			$("#button-send").attr("src", "img/send_energy_off.png");
+			$("#house-alt").attr("src", "img/house_alt_off.png");
+
+		}, 800);
+
+	})
+
+	$("#button-send-ibm").click(function() {
+
+		$("#button-send-ibm").attr("src", "img/send_to_ibm_on.png");
+
+		var timer_1 = setTimeout(function() {
+
+			$("#button-send-ibm").attr("src", "img/send_to_ibm_off.png");
+			$("#button-send-comp").attr("src", "img/send_to_ibm_on.png");
+
+		}, 800);
+
+		var timer_2 = setTimeout(function() {
+			$("#button-send-comp").attr("src", "img/send_to_ibm_off.png");
+
+		}, 1600);
+
 	})
 	
-	$("#button-send").click(function(){
-		
-		$("#button-send").attr("src","img/send_energy_on.png");
-		$("#house-alt").attr("src","img/house_alt_on.png");
-		
-		var timer = setTimeout(function(){ 
-			
-			$("#button-send").attr("src","img/send_energy_off.png");
-			$("#house-alt").attr("src","img/house_alt_off.png");
-		
-		}, 800); 
-		
-		
+	$("#button-return").click(function() {
+
+		$("#button-return").attr("src", "img/return_on.png");
+
+		var timer = setTimeout(function() {
+
+			$("#button-return").attr("src", "img/return_off.png");
+
+		}, 800);
+
 	})
-	
-	
+
 });
